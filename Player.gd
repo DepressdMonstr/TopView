60582a20ea4cd6398387013bd19aecdd925c86d6
yenthe7575 <yenthe123jannis@gmail.com>
2022-04-28 09:15:06 +0200

    Movement

---
+++ Player.gd
@@ -0,0 +1,39 @@
+extends KinematicBody2D
+
+var curHp : int = 10
+var maxHp : int = 10
+var moveSpeed : int = 250
+var damage : int = 1
+var interactDist : int = 70
+var vel : Vector2 = Vector2()
+var facingDir : Vector2 = Vector2()
+onready var rayCast = get_node("RayCast2D")
+
+func _ready():
+	pass # Replace with function body.
+
+func _physics_process(delta):
+  
+  vel = Vector2()
+  
+  # inputs
+  if Input.is_action_pressed("move_up"):
+	vel.y -= 1
+	facingDir = Vector2(0, -1)
+	
+  if Input.is_action_pressed("move_down"):
+	vel.y += 1
+	facingDir = Vector2(0, 1)
+	
+  if Input.is_action_pressed("move_left"):
+	vel.x -= 1
+	facingDir = Vector2(-1, 0)
+	
+  if Input.is_action_pressed("move_right"):
+	vel.x += 1
+	facingDir = Vector2(1, 0)
+  
+  vel = vel.normalized()
+  
+  # move the player
+  move_and_slide(vel * moveSpeed)
